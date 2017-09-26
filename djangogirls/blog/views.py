9dbdf44f4b4ea841af028a5896dbee9a14a2a882
LeeHanYeong <dev@azelf.com>
2017-09-26 12:09:48 +0900

    post_detail view추가

--- djangogirls/blog/views.py
+++ djangogirls/blog/views.py
@@ -11,3 +11,24 @@ def post_list(request):
         'posts': posts,
     }
     return render(request, 'blog/post_list.html', context)
+
+
+# View(Controller) 구현
+# post_detail기능을 하는 함수를 구현
+# 'post'라는 key로 Post.objects.first()에 해당하는 Post객체를 전달
+# 템플릿은 'blog/post_detail.html'을 사용
+def post_detail(request):
+    # Post인스턴스 1개만 가져옴, 변수명은 posts가 아닌 단일객체를 나타내는 post사용
+    post = Post.objects.first()
+    # 'post'key값으로 Post인스턴스 하나 전달
+    context = {
+        'post': post,
+    }
+    return render(request, 'blog/post_detail.html', context)
+
+# Template(View) 구현
+# 실제 템플릿파일 생성
+# 'post'라는 변수를 이용해 Post객체의 내용을 출력
+
+# UrlResolver(urls.py)
+# /post/detail/ url을 'post_detail'뷰와 연결
